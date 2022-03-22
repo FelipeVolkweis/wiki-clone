931a6ea46cfd781a455a3baea99c6801055e3681
FelipeVolkweis <98425460+FelipeVolkweis@users.noreply.github.com>
2022-03-22 16:37:08 -0300

    Delete some.py

--- some.py
+++
@@ -1,6 +0,0 @@
-import re
-
-content = "# CSS and java\r\n\nCSS is a language that can be used to add style to an [HTML](/wiki/HTML) page."
-header = re.search("^# .*", content)
-result = header.group()[:-1][2:]
-print(result)
